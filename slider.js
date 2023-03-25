70758ace0054857f49f43095b0f7127453d02366
franvg04 <68405361+franvg04@users.noreply.github.com>
2023-03-25 02:38:55 -0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,30 @@
+const sliderContent = document.querySelectorAll('.slider-content')
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+let index = 2
+
+
+const mount = () => sliderContent[index].classList.add('active')
+const unmount = () => sliderContent[index].classList.remove('active')
+
+next.addEventListener('click', ()=> {
+    unmount()
+    if (index >= sliderContent.length - 1){
+        index = 0
+    } else {
+        index++
+    }
+    mount()
+})
+
+prev.addEventListener('click', ()=> {
+    unmount()
+    if (index <= 0){
+        index = sliderContent.length - 1
+    } else {
+        index--
+    }
+    mount()
+})
+
+mount()
