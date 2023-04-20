8cdabcad8fd909773a0b2f7d386647e65e292720
franvg04 <68405361+franvg04@users.noreply.github.com>
2023-04-20 14:26:06 -0300

    Update sliderB.js

--- sliderB.js
+++ sliderB.js
@@ -1,7 +1,7 @@
 const sliderContent = document.querySelectorAll('.slider-content')
 const next = document.querySelector('.next')
 const prev = document.querySelector('.prev')
-let index = 4
+let index = 5
 
 
 const mount = () => sliderContent[index].classList.add('active')
